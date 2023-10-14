cc191497819d7cf09a88369bc282e7366bd13ace
Adityya Kaushal <adityyakaushal@gmail.com>
2023-10-14 15:06:37 -0400

    Added accordions

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Hello</h1>
+<script>
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="m-20">
+	<Accordion autocollapse>
+		<AccordionItem open>
+			<svelte:fragment slot="lead">1</svelte:fragment>
+			<svelte:fragment slot="summary">Today's Task</svelte:fragment>
+			<svelte:fragment slot="content">Hallo</svelte:fragment>
+		</AccordionItem>
+		<AccordionItem open>
+			<svelte:fragment slot="lead">1</svelte:fragment>
+			<svelte:fragment slot="summary">Today's Task</svelte:fragment>
+			<svelte:fragment slot="content">Hallo</svelte:fragment>
+		</AccordionItem>
+	</Accordion>
+</div>
