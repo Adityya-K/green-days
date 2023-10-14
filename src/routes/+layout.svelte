240fe391403979d393324495312b4d9c5fbc92e8
Adityya Kaushal <adityyakaushal@gmail.com>
2023-10-14 12:08:37 -0400

    Added Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -8,42 +8,30 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+<AppBar>
+	<svelte:fragment slot="lead">
+		<img width="50px" height="50px" src="src/logo.png" />
 	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+	<h1 class="h1">Green Days</h1>
+	<svelte:fragment slot="trail">
+		<ul class="links">
+			<li><a href="/"> Home </a></li>
+			<li><a href="/tasks"> Daily Tasks </a></li>
+		</ul>
+	</svelte:fragment>
+</AppBar>
+<slot />
+
+<style>
+	.links {
+		display: flex;
+	}
+
+	li {
+		padding-left: 15px;
+	}
+
+	.image {
+		height: 20%;
+	}
+</style>
